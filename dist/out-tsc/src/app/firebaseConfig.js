0b7390ab8bb143ebc4fcc5a9d8d9321e3f04c8da
Saul Shanabrook <s.shanabrook@gmail.com>
2016-12-01 01:53:04 -0500

    Update generated files

--- dist/out-tsc/src/app/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: 'AIzaSyCb1UK_VEsGXhYAe3iI5mTdrm83d_8Dl3o',
-    authDomain: 'file-share-890fe.firebaseapp.com',
-    databaseURL: 'https://file-share-890fe.firebaseio.com',
-    storageBucket: 'file-share-890fe.appspot.com',
-    messagingSenderId: '903868047800'
-};
-//# sourceMappingURL=../../../../src/app/firebaseConfig.js.map
--- dist/out-tsc/src/app/models.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=../../../../src/app/models.js.map
